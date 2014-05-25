2508e3586c61a3f7aa6f6860bb492e15f042fe3b
Tyler McGinnis <tylermcginnis33@gmail.com>
2014-05-25 13:57:43 -0600

    Add more questions and alter modal CSS

--- server-assets/questionData.js
+++ server-assets/questionData.js
@@ -8,6 +8,26 @@ var questionData = [
     question: 'Have the function "longest" take a string as the parameter and returns the longest word in that string.',
     fn: 'var longest = function(str){\n\n}',
     answer: 'spaceship'
+  },
+  {
+    question: 'Have the function "dyslexicYoda" take a string as the parameter and return that string after you reverse the order of the words. ie "I enjoy eating cold meatballs" becomes "meatballs cold eating enjoy I"',
+    fn: 'var dyslexicYoda = function(str){\n\n}',
+    answer: 'toys his enjoys Leo'
+  },
+  {
+    question: 'Have the function "vowelCount" take a string and return how many vowels are in that string',
+    fn: 'var vowelCount = function(str){\n\n}',
+    answer: 9
+  },
+  {
+    question: 'Have the function "palindrome" take a string and return true if that string is a palindrome (written the same forward and backwards)',
+    fn: 'var palindrome = function(str){\n\n}',
+    answer: true
+  },
+  {
+    question: 'Have the function "titleMaker" take a string and return that string after capitalizing the first letter in every word.',
+    fn: 'var titleMaker = function(str){\n\n}',
+    answer: 'Peter Piper Picked Peppers and Run Rocked Rhymes'
   }
 ];
 
